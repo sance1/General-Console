b4c4f4d8d15d482733523dd27337a6dec7d41783
DESKTOP-9QLCHAS\sance <sanceyff@gmail.com>
2026-06-14 03:58:34 +0700

    add console portable

--- C# PortableConsole/TestProgram.cs
+++ C# PortableConsole/TestProgram.cs
@@ -5,12 +5,28 @@ class Program
 {
     static void Main()
     {
+		int n = 6;
+		for(int i = 1; i <= n; i++)
+		{
+			Console.WriteLine(i);
+			 string spasi = new string('-', n - i);
+			 string kar = new string('x', i*2-1);
+			Console.WriteLine(spasi+kar);
+		}
+		Console.WriteLine(" ");
+		Console.WriteLine("ok");
+		string ok = new string('t', 6);
+		Console.WriteLine(ok);
+		Console.ReadKey();
+    }
+	static void Test1()
+	{
 		Console.WriteLine("Modified oke");
 		List<int> arr = new List<int>{10,20,10,20,30};
 		int result = Proses(5,arr);
 		Console.WriteLine(result);
         Console.ReadKey();
-    }
+	}
 	static int Proses(int n, List<int> arr)
 	{
 		int result = 0;
--- GeneralConsole/obj/Release/net10.0/GeneralConsole.AssemblyInfo.cs
+++ GeneralConsole/obj/Release/net10.0/GeneralConsole.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeneralConsole")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a0e55053a81d50eea5055e287669474a3b1e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeneralConsole")]
